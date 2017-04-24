b9df411c448a944250ac33f25453d47499cb43d9
matthewmanning <matt.manning@parcishipping.com>
2017-04-24 05:21:22 -0400

    sql and pysql integration

--- a.py
+++ a.py
@@ -8,10 +8,10 @@ from pymysql.cursors import DictCursor
 import json
 
     ##Connect to the database
-connection = pymysql.connect(host='wackystat2.cpdisqlre7yl.us-west-2.rds.amazonaws.com',
-user='manning1234',
-password='Bisnet1234',
-db='wackystat2',
+connection = pymysql.connect(host='db4free.net',
+user='eightypiusername',
+password='Bisnet123',
+db='eightypidatabase',
 charset='utf8mb4',
 cursorclass=DictCursor)
 
@@ -23,7 +23,7 @@ def index():
         with connection.cursor() as cursor:
         # Create a new record
             sql = "INSERT INTO `auth` (`dbid`, `keyid`, `secretid`, `lastupdate`) VALUES (%s,%s,%s, %s)"
-            cursor.execute(sql, ('44-aaa','1234', 'very-secret', '12-22-09'))
+            cursor.execute(sql, ('44','1234', 'very-secret', '12-22-09'))
 
     # connection is not autocommit by default. So you must commit to save
     # your changes.
@@ -40,19 +40,20 @@ def id_generator(size=32, chars=string.ascii_uppercase + string.digits):
 import uuid
 def uuid_generator():
     return uuid.uuid4()
+
 @app.route('/get/', methods=['POST','GET'])
 def get():
     print(id_generator())
     print(uuid_generator())
-    key = "2017-03-23 05:55:28"
+    key = "1234"
     try:
         with connection.cursor() as cursor:
         # Read a single record
-            sql = "SELECT * FROM `auth` WHERE `lastupdate` =< %s"
+            sql = "SELECT `secretid` FROM `auth` WHERE `keyid` = %s"
             cursor.execute(sql, (key,))
             #result = cursor.fetchall()
             json_result = cursor.fetchone().values()
-            solve = str(json_result[2])
+            solve = str(json_result[0])
 
     finally:
         connection.close()
@@ -61,4 +62,9 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-
+#SQL SETTINGS FOR CREATING DB
+#CREATE TABLE `auth`(secretid TEXT,
+#  dbid INT AUTO_INCREMENT,
+#  keyid VARCHAR(100) UNIQUE,
+#  lastupdate TEXT,
+#  PRIMARY KEY (dbid));
--- main.py
+++ main.py
@@ -19,9 +19,12 @@ app = Flask(__name__)
 #     TIMESTAMPING LOGIC
 ###############################
 
-now = datetime.datetime.now()
-utctime = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") #Variable is set to current time it is pulling from
+#now = datetime.datetime.now() #retrieve the current time from datetime module
+#utctime = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") #Variable is set to current time it is pulling from
 
+def utctime():
+
+    return datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")  # Variable is set to current time it is pulling from
 ###############################
 #     RANDOM VALUE GENERATORS
 ###############################
@@ -35,12 +38,19 @@ def uuid_generator(): #Can be used globally to return a random uuid
 #############################
 #     SQL DATABASE SETTINGS
 #############################
-connection = pymysql.connect(host='wackystat2.cpdisqlre7yl.us-west-2.rds.amazonaws.com',
-user='manning1234',
-password='Bisnet1234',
-db='wackystat2',
+
+connection = pymysql.connect(
+
+host='db4free.net',
+user='eightypiusername',
+password='Bisnet123',
+db='eightypidatabase',
 charset='utf8mb4',
-cursorclass=pymysql.cursors.DictCursor)
+cursorclass=pymysql.cursors.DictCursor,
+autocommit=True,
+
+)
+
 #############################
 #     EXTERNAL API KEYS
 #############################
@@ -53,78 +63,102 @@ weather_key = "894eb3084dd61ab649bf20494bd3cacb"
 #############################
 @app.route('/core/<key>', methods=['POST','GET'])
 def create_task(key):
-    ###
-    #SECURITY/AUTHENTICATION
-    ###
-    #
 
-    #
-    ###
+    ########################
+    #SECURITY/AUTHENTICATION
+    ########################
+
+    try:
+        with connection.cursor() as cursor: #begin connection
+        # Read the secret id from the database
+            sql = "SELECT `secretid` FROM `auth2` WHERE `keyid` = %s"
+            cursor.execute(sql, (key,))
+            #result = cursor.fetchall()
+            json_result = cursor.fetchone().values() #secretid/query result in JSON format
+            raw_secret = str(json_result[0]) #raw secretid
+    finally:
+        connection.cursor()
+
+    confirm_secret = raw_secret #secret from database
+    # secret requested by user in body of request
+    secret_req = request.json.get("secretid", "")
+
+    if secret_req != confirm_secret: #if the secret from the db and secrect request dont match the service will abort
+        abort(400)
+
+
+    ###############################################
     #CORE REQUESTED DATA FROM BODY IN JSON POST CALL
-    ###
-    #
+    ################################################
+
     latlng = request.json.get("latlng", "") #Requests the user's current latlng (location) in a string format Ex. "123,784"
     userid = request.json.get("userid", "")  #Request the user's unique id in a string format
     sessionid = request.json.get("userid", "") #Request the session id of user
     raw_sessionopen = request.json.get("sessionopen", "")  #Optional request of the time the session began
 
-    ####
+    #########################
     #Session ID Time Stamping
-    ####
+    #########################
+
+    liveutctime = utctime() #converts global function to a callabale variable
     #If a sessioon open time is not supplied in the body of the request, then system sets sessionoppen to UTC time/date
     if raw_sessionopen == "": #The session open request is empty
-        sessionopen = utctime
+        sessionopen = liveutctime
     else: #he session open request has data
         sessionopen = raw_sessionopen
 
-    ###
+    ####################
     #BODY REQUEST ERRORS
-    ###
+    ####################
 
     #if latlng or userid or sessionid is None:
     if latlng and userid and sessionid == "":
-        abort(400) #410 error did not supply latlng or sessionid or userid
+        abort(400) #400 error did not supply latlng or sessionid or userid
 
     #WEATHER DATA
-    current_lng = latlng.rsplit(',', 1)[0]
-    current_lat = latlng.rsplit(',', 1)[1]
-    response = requests.get("http://api.openweathermap.org/data/2.5/weather?lat="
-                            + current_lat + "&lon=" + current_lng + "&appid=" + weather_key + "")
-    json_object = response.json()
-    temp = round(float(json_object['main']['temp']))
+    #current_lng = latlng.rsplit(',', 1)[0]
+    #current_lat = latlng.rsplit(',', 1)[1]
+    #response = requests.get("http://api.openweathermap.org/data/2.5/weather?lat="
+    #                        + current_lat + "&lon=" + current_lng + "&appid=" + weather_key + "")
+    #json_object = response.json()
+    #temp = round(float(json_object['main']['temp']))
 
     #RESPONSE DATA
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'userid': userid,
-        'sessionid': sessionid,
-        'latlng': latlng,
-        'done': False,
-        'key': key
-    }
+
     core = {
         'uid': coredata[-1]['uid'] + 1,
-        'temperature': temp,
+        #'temperature': temp,
         'userid': userid,
         'latlng': latlng,
         'session_data': { 'sessionopen': sessionopen,
                          'sessionid': sessionid },
         'key': key
     }
-    tasks.append(task)
-    coredata.append(core)
-    #return jsonify({'task': task}), 200
+    # append data to actual response VERY IMPORTANT
+    coredata.append(core)  #coredata (preset response) #append attaches new data #(core) new data above
+
+
+    ###################
+    #SAVE DATA TO MYSQL
+    ###################
+
+    try:
+        with connection.cursor() as cursor:
+        # Create a new record
+            sql = "INSERT INTO `users` (`userid`, `keyid`, `latlng`, `sessionid`, `sessionopen`, `lastupdate`) VALUES (%s, %s, %s, %s, %s, %s)"
+            cursor.execute(sql, (userid,key,latlng,sessionid,sessionopen,liveutctime))
+
+    # connection is not autocommit by default. So you must commit to save
+    # your changes.
+        connection.commit()
+        #connection.close()
+
+    finally:
+        print("Succesfully registered user at " + liveutctime)
+
+
     return jsonify({'core': core}), 201
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-
-]
 
 coredata = [
     {
@@ -139,24 +173,27 @@ def get_tasks():
 
 @app.route('/preregister', methods=['GET'])
 def preregister():
-    utctime
+    utctime()
     return render_template('preregister.html',time=utctime)
 
 @app.route('/register', methods=['GET','POST'])
 def register():
-    regiser_key = str(id_generator())
-    register_secretid = str(uuid_generator())
+    liveutctime = str(utctime()) #run utctime global function
+    regiser_key = str(id_generator()) #run idgenerator global function
+    register_secretid = str(uuid_generator()) #run uuid global function
     try:
         with connection.cursor() as cursor:
         # Create a new record
-            sql = "INSERT INTO `auth` (`dbid`, `keyid`, `secretid`, `lastupdate`) VALUES (%s, %s, %s, %s)"
-            cursor.execute(sql, (regiser_key, regiser_key, register_secretid, utctime))
+            sql = "INSERT INTO `auth2` (`dbid`, `keyid`, `secretid`, `lastupdate`) VALUES (%s, %s, %s, %s)"
+            cursor.execute(sql, (regiser_key, regiser_key, register_secretid, liveutctime))
 
     # connection is not autocommit by default. So you must commit to save
     # your changes.
         connection.commit()
+        #connection.close()
 
-        print("Succesfully registered user at " + utctime)
+        return ("Succesfully registered user at " + liveutctime)
+
     except:
         print("failure")
     return str(regiser_key), str(register_secretid)
